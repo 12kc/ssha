31e7b026d7de4be0a8620025429eabd72d8c2a71
kalani <82015430+12kc@users.noreply.github.com>
2024-10-21 18:06:11 -1000

    Unfinished but in "working" state

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/12kc/ssha
+
+go 1.22.5
--- main.go
+++ main.go
@@ -3,40 +3,51 @@ package main
 import (
 	"fmt"
 	"math"
-	"strconv"
 )
 
-var t = [62]rune{
-	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-	'i', 'j', 'k', 'l', 'm', 'n', 'o',
-	'p', 'q', 'r', 's', 't', 'u', 'v',
-	'w', 'x', 'y', 'z', 'A', 'B', 'C',
-	'D', 'E', 'F', 'G', 'H', 'I', 'J',
-	'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-	'R', 'S', 'T', 'U', 'V', 'W', 'X',
-	'Y', 'Z', '1', '2', '3', '4', '5',
-	'6', '7', '8', '9', '0',
-}
-
-var out = [32]uint8{
-	28, 15, 6, 31, 17, 22, 55, 23, 9,
-	10, 13, 25, 3, 26, 19, 11, 4, 29,
-	5, 24, 18, 21, 7, 16, 2, 30, 12,
-	32, 14, 8, 42, 58,
-}
+var (
+	t = [62]rune{
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'A', 'B', 'C',
+		'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', '1', '2', '3', '4', '5',
+		'6', '7', '8', '9', '0',
+	}
+	out = [32]uint8{
+		28, 15, 6, 31, 17, 22, 55, 23, 9,
+		10, 13, 25, 3, 26, 19, 11, 4, 29,
+		5, 24, 18, 21, 7, 16, 2, 30, 12,
+		32, 14, 8, 42, 58,
+	}
+	input   string
+	overlap int = len(input) - 32
+)
 
 func main() {
-	var current float64 = 24
-	var nextChar float64 = 38
+	var current float64
+	var nextChar float64
 
-	// Math shit
+	fmt.Scan(&input)
+	// Check what place in table
+	for i, v := range t {
+		if v == input[i] {
+			current, nextChar = i, input[i+1]
+		}
+	}
+
+	for i, l := 0, len(input); i < l+overlap; i++ {
+		c := hash(current, nextChar)
+		out[i] = (out[i] + uint8(c)) % 62
+	}
+}
+
+func hash(current float64, nextChar float64) int {
 	current *= math.Sin(129) + math.Sqrt(9391)/math.Log10(nextChar)
 	current += math.Mod(math.Pow(nextChar, 29)+math.Sqrt(current/4), 2901)
 	current = math.Mod(current, 62)
-	current = math.Abs(current)
-
-	var current2 int
-	current2 = int(math.Mod(current, 62))
-
-	fmt.Println(strconv.Itoa(current2))
+	return int(current)
 }
