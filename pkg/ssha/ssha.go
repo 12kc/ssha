7dbe632cc93b17a93816f41d362983464f360ebb
Kalani Cunha <cunhakalani3@gmail.com>
2024-11-05 11:57:13 -1000

    finally working

--- pkg/ssha/ssha.go
+++ pkg/ssha/ssha.go
@@ -1,10 +1,12 @@
 package ssha
 
-import "math"
+import (
+	"math"
+)
 
 // This is all very messy, it wasn't meant to be good, much less be readable ;)
 
-func Hash(in string) [32]uint8 {
+func Hash(in string) string {
 	var t = [62]rune{ // Table of chars available, for input/output
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o',
@@ -16,20 +18,20 @@ func Hash(in string) [32]uint8 {
 		'Y', 'Z', '1', '2', '3', '4', '5',
 		'6', '7', '8', '9', '0',
 	}
-	var out = [32]uint8{ // Template string to change, 128 bits long
+	var out = [32]int{ // Template string to change, 128 bits long
 		28, 15, 6, 31, 17, 22, 55, 23, 9,
 		10, 13, 25, 3, 26, 19, 11, 4, 29,
 		5, 24, 18, 21, 7, 16, 2, 30, 12,
 		32, 14, 8, 42, 58,
 	}
-	inMap := make([]int, 0) // Used to simplify table usage
+	inS := make([]int, 0) // Used to simplify table usage
 	overlap := len(in) - 32
 
 	// Check if input is in table
 	for _, char := range in {
 		for j, runeVal := range t {
 			if runeVal == char {
-				inMap = append(inMap, j)
+				inS = append(inS, j)
 				break
 			}
 		}
@@ -37,14 +39,14 @@ func Hash(in string) [32]uint8 {
 
 	// Hash characters from in
 	for i, lenI := 0, len(in); i < lenI; i++ {
-		c := float64(inMap[i])
+		c := float64(inS[i])
 
 		// Math processes
 		c *= math.Sin(129) + math.Sqrt(9391)/math.Log10(c)
 		c += math.Mod(math.Pow(c, 29)/math.Sqrt(3), 17)
 		c = math.Mod(c, 255)
 
-		inMap[i] = int(c)
+		inS[i] = int(c)
 	}
 
 	// Fix overlap before string processing
@@ -61,12 +63,20 @@ func Hash(in string) [32]uint8 {
 	*/
 	for i := 0; i < len(out); i++ {
 		for j := 0; j < len(in); j++ {
-			if inMap[j] < 128 {
-				out[i] = (out[i] + uint8(inMap[j])) % 62
+			if inS[j] < 128 {
+				out[i] = (out[i] + inS[j]) % 62
 			} else {
-				out[i] = (out[i] - uint8(inMap[j])) % 62
+				out[i] = (out[i] - inS[j]) % 62
 			}
 		}
 	}
-	return out
+
+	outS := make([]rune, len(out))
+	// Process into rune slice
+	for i := 0; i < len(out); i++ {
+		outS[i] = t[int(math.Abs(float64(out[i])))]
+	}
+
+	output := string(outS)
+	return output
 }
